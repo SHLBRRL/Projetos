bb4593bb2b492038cb9367b6cd716e2cc941c0f3
Enrico <rico500000@gmail.com>
2022-10-17 15:40:03 -0300

    Integrantes

--- Compiladores/SimpleExpr/src/parser/Main.java
+++ Compiladores/SimpleExpr/src/parser/Main.java
@@ -1,4 +1,11 @@
-/*
+/*  
+    Integrantes:
+
+    Enrico Giannobile                RA: 19.00610-0
+    Guilherme de Campos              RA: 20.00089-8
+    Luis Guilherme de Souza Munhoz   RA: 20.01937-8
+
+    
     Para executar o interpretador na linha de comando (fora do IDE):
         java -jar simple_expr.jar
  */
