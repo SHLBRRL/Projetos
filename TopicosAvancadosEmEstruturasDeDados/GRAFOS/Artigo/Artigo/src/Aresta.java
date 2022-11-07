ef2e5aa04934417f6817c9695f28fc03434518e1
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-11-07 11:27:22 -0300

    adicionando pesos nas arestas

--- TopicosAvancadosEmEstruturasDeDados/GRAFOS/Artigo/Artigo/src/Aresta.java
+++ TopicosAvancadosEmEstruturasDeDados/GRAFOS/Artigo/Artigo/src/Aresta.java
@@ -1,15 +1,19 @@
 public class Aresta {
     Vertice origem;
     Vertice destino;
-    
-    public Aresta(Vertice origem, Vertice destino) {
+    Integer peso;
+
+    public Aresta(Vertice origem, Vertice destino, Integer peso) {
         this.origem = origem;
         this.destino = destino;
+        this.peso = peso;
     }
 
     @Override
     public String toString() {
-        return "Aresta [origem=" + origem + ", destino=" + destino + "]";
+        return "Aresta [origem=" + origem + ", destino=" + destino + ", peso=" + peso + "]";
     }
+
+
 }
     
--- TopicosAvancadosEmEstruturasDeDados/GRAFOS/Artigo/Artigo/src/Teste_Grafo.java
+++ TopicosAvancadosEmEstruturasDeDados/GRAFOS/Artigo/Artigo/src/Teste_Grafo.java
@@ -9,24 +9,24 @@ public class Teste_Grafo {
         Vertice V5 = new Vertice("Poços de Caldas");
         Vertice V6 = new Vertice("BatCaverna");
 
-        V1.addAresta(new Aresta(V1, V2));
-        V1.addAresta(new Aresta(V1, V5));
+        V1.addAresta(new Aresta(V1, V2,5));
+        V1.addAresta(new Aresta(V1, V5,4));
 
-        V2.addAresta(new Aresta(V2, V1));
-        V2.addAresta(new Aresta(V2, V3));
-        V2.addAresta(new Aresta(V2, V4));
-        V2.addAresta(new Aresta(V2, V5));
+        V2.addAresta(new Aresta(V2, V1,5));
+        V2.addAresta(new Aresta(V2, V3,3));
+        V2.addAresta(new Aresta(V2, V4,2));
+        V2.addAresta(new Aresta(V2, V5,7));
         
-        V3.addAresta(new Aresta(V3, V2));
-        V3.addAresta(new Aresta(V3, V4));
+        V3.addAresta(new Aresta(V3, V2,3));
+        V3.addAresta(new Aresta(V3, V4,8));
 
-        V4.addAresta(new Aresta(V4, V2));
-        V4.addAresta(new Aresta(V4, V3));
-        V4.addAresta(new Aresta(V4, V5));
+        V4.addAresta(new Aresta(V4, V2,2));
+        V4.addAresta(new Aresta(V4, V3,8));
+        V4.addAresta(new Aresta(V4, V5,9));
 
-        V5.addAresta(new Aresta(V5, V1));
-        V5.addAresta(new Aresta(V5, V2));
-        V5.addAresta(new Aresta(V5, V4));
+        V5.addAresta(new Aresta(V5, V1,4));
+        V5.addAresta(new Aresta(V5, V2,7));
+        V5.addAresta(new Aresta(V5, V4,9));
         
         g.addVertice(V1);
         g.addVertice(V2);
